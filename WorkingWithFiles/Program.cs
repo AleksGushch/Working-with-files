446687f689932892e3a963e5c7e1d1f99295dd56
AleksGushch <alexg1703@mail.ru>
2023-04-22 12:56:59 +0300

    added check existing file

--- WorkingWithFiles/Program.cs
+++ WorkingWithFiles/Program.cs
@@ -24,21 +24,20 @@ namespace WorkingWithFiles
                 byte height = Convert.ToByte(Console.ReadLine());
                 Console.Write("Введите дату рождения сотрудника(дд/мм/гггг): ");
                 string dateOfBirth = Console.ReadLine();
-                DateTime parseOfBirth = DateTime.Parse(dateOfBirth, DateTimeFormatInfo.CurrentInfo);
+                DateTime parseOfBirth = DateTime.Parse(dateOfBirth);
                 Console.Write("Введите место рождения сотрудника: ");
                 string placeOfBirth = Console.ReadLine();
-                sw.WriteLine($"{id++}#{age}#{height}#{parseOfBirth}#{placeOfBirth}#{date}#{time}");
+                sw.WriteLine($"{id++}#{age}#{height}#{parseOfBirth.ToString("D")}#{placeOfBirth}#{date}#{time}");
             }
         }
         static void Reader() 
         {
-            using (StreamReader sr = new StreamReader("Сотрудники.txt",Encoding.Unicode)) 
+            using (StreamReader sr = new StreamReader("Сотрудники.txt", Encoding.Unicode)) 
             {
                 string infoAboutEmployee;
                 while ((infoAboutEmployee = sr.ReadLine()) != null) 
                 {
-                    const string V = "#";
-                    string[] dataAboutEmployee = infoAboutEmployee.Split(Convert.ToChar(V));
+                    string[] dataAboutEmployee = infoAboutEmployee.Split(Convert.ToChar("#"));
                     foreach (string word in dataAboutEmployee) 
                     {
                         Console.Write(" "+ word);
@@ -49,10 +48,11 @@ namespace WorkingWithFiles
         }
         static void Main(string[] args)
         {
+            string path = Directory.GetCurrentDirectory();
             bool check=true;
             while(check)
             {
-                Console.Write("Хотите внести данные(1) или посмотреть данные(2) или выйти (любая клавиша)?: ");
+                Console.Write("Хотите внести данные(1) или посмотреть данные(2) или выйти (0)?: ");
                 byte input = Convert.ToByte(Console.ReadLine());
                 if (input == 1)
                 {
@@ -60,7 +60,15 @@ namespace WorkingWithFiles
                 }
                 else if (input == 2)
                 {
-                    Reader();
+                    if (File.Exists(path+@"\Сотрудники.txt"))
+                    {
+                        Reader();
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Файл 'Сотрудники.txt' не найден, создайте новый файл");
+                        Writer();
+                    }
                 }
                 else 
                 {
